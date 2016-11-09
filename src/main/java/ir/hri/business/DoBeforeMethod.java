768e5cfb2fd72dc280b18890373c7efeedbf89bf
HR <ireh.hr@gmail.com>
2016-11-09 14:26:41 +0330

    Initial commit

---
+++ src/main/java/ir/hri/business/DoBeforeMethod.java
@@ -0,0 +1,14 @@
+package ir.hri.business;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public class DoBeforeMethod implements MethodBeforeAdvice {
+
+    public void before(Method method, Object[] args, Object target)
+
+            throws Throwable {
+        System.out.println("****SPRING AOP**** DoBeforeMethod : Executing before method!");
+    }
+}
---
+++ src/main/java/ir/hri/view/App.java
@@ -0,0 +1,23 @@
+package ir.hri.view;
+
+import ir.hri.business.SimpleService;
+import ir.hri.util.SpringUtil;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        SimpleService simpleService = (SimpleService) SpringUtil.getApplicationContext().getBean("simpleServiceProxy");
+        simpleService.printNameId();
+        System.out.println("--------------");
+        try {
+            simpleService.checkName();
+        } catch (Exception e) {
+            System.out.println("SimpleService: Method checkName() exception thrown..");
+        }
+        System.out.println("--------------");
+        simpleService.sayHello("Ireh");
+    }
+}
